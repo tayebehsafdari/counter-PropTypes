79317468ce35925fcc509aedc7e8cd7a8bb7f1fd
tayebehsafdari <66350369+tayebehsafdari@users.noreply.github.com>
2021-08-16 14:51:34 +0430

    modify files

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,8 +1,11 @@
 import React, {useState} from "react";
+import PropTypes from "prop-types";
 
-function Counter({initialCount}) {
+const Counter = React.memo(function Counter({initialCount}) {
     const [count, setCount] = useState(initialCount);
-
+    /* static defaultProps = {
+        name: 'Counter'
+    }; */
     return (
         <div className="input-group my-5">
             <span className="input-group-text" onClick={() => setCount(initialCount)}>Reset</span>
@@ -12,6 +15,14 @@ function Counter({initialCount}) {
             <span className="input-group-text" onClick={() => setCount(count + 1)}>+</span>
         </div>
     );
-}
+});
+
+Counter.propTypes = {
+    initialCount: PropTypes.number.isRequired
+};
+
+Counter.defaultProps = {
+    name: 'Counter'
+};
 
 export default Counter;
